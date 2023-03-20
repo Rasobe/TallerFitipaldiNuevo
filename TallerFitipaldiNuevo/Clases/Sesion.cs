2181044bfca67f28401f666f9ba9b376c6a27b75
Raúl Solano <rasoberdiel3@gmail.com>
2023-03-20 09:48:07 +0100

    algunas pequeñas modificaciones (poder minimizar algunas ventanas)

--- TallerFitipaldiNuevo/Clases/Sesion.cs
+++ TallerFitipaldiNuevo/Clases/Sesion.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TallerFitipaldiNuevo.Clases
+﻿namespace TallerFitipaldiNuevo.Clases
 {
     public class Sesion
     {
